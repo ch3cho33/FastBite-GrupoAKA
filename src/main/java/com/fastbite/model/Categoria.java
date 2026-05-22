d0da3c4c8fd952c2b6c900262f0d7d7abdde8203
zeuznnss <nassinsuz@gmail.com>
2026-05-21 19:41:49 -0500

    Update Categoria.java

--- src/main/java/com/fastbite/model/Categoria.java
+++ src/main/java/com/fastbite/model/Categoria.java
@@ -1,26 +1,31 @@
-package com.fastbite.fastbite.model;
+package com.fastbite.model;
 public class Categoria {
     private int id;
     private String nombre;
-
     public Categoria() {
     }
+    
     public Categoria(int id, String nombre) {
         this.id = id;
         this.nombre = nombre;
     }
+    
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public String getNombre() {
         return nombre;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
     @Override
     public String toString() {
         return nombre;
